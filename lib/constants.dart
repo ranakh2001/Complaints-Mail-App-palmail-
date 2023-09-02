34920ac4240737a31b9981542bed3bdf3979d64c
Rana El-khodary <77431301+ranakh2001@users.noreply.github.com>
2023-09-02 12:06:43 +0300

    init commit

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+// api links
+import 'package:flutter/material.dart';
+
+const String baseUrl = "https://palmail.gsgtt.tech/api";
+//add the endpoints that you use (لانهم كتار)
+
+// colors
+Color kprimaryColor = const Color(0xff003AFC);
+Color kgrdiantC1 = const Color(0xff6589FF);
+Color kgrdiantC2 = const Color(0xff003AFC); // same the primary color
+Color ksecondaryColor = const Color(0xff7C7C7C);
+Color kinBoxStatus = const Color(0xffFA3A57);
+Color kpendingStatus = const Color(0xffFFE120);
+Color kinProgressStatus = const Color(0xff6589FF);
+Color ktitleBlack = const Color(0xff272727);
+Color kiconColor = const Color(0xffB2B2B2);
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+void main()async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(
+      path: 'assets/translation',
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      fallbackLocale: const Locale('en'),
+      child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+            debugShowCheckedModeBanner: false,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      title: 'PalMail',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+
+    );
+  }
+}
